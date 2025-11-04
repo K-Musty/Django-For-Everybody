5d6f6eb358236d4e66d2f5343ee9495f17a4c50a
K-Musty <kmustapha9564@gmail.com>
2025-11-04 20:37:31 +0100

    Hello world to run locally

--- 15-hello-world/home/urls.py
+++ 15-hello-world/home/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
-from . import views
-from django.contrib.auth import views as auth_views
+from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('', views.HomeView.as_view()),
+    path('', TemplateView.as_view(template_name='home/hello.html')),
 ]
